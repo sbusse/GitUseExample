02640897883222e1381f56b7c03d5fffd245822a
Susan Busse Kersting <31662073+sbusse@users.noreply.github.com>
2020-09-02 14:27:13 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by student on 9/2/20.
+//  Copyright © 2020 MAD 257. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitUseExampleTests/GitUseExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitUseExampleTests.swift
+//  GitUseExampleTests
+//
+//  Created by student on 9/2/20.
+//  Copyright © 2020 MAD 257. All rights reserved.
+//
+
+import XCTest
+@testable import GitUseExample
+
+class GitUseExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
