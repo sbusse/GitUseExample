ebb2228198abe5e9521d84eb8567682eecfd9447
Susan Busse Kersting <31662073+sbusse@users.noreply.github.com>
2020-09-03 13:55:02 -0500

    This is my first commit with Xcode to Git

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
