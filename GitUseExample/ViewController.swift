0fe810290ef70cc4f6ce1d3e77a0af24a493a7f3
Susan Busse Kersting <31662073+sbusse@users.noreply.github.com>
2020-09-03 15:32:19 -0500

    You finished commit

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You finished!!")
     }
     
     override func viewDidLoad()
